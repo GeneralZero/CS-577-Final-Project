716e8339530f3386b7608b7da212347468ca342c
Brian Ridings <bdrid001@gmail.com>
2014-12-10 05:55:38 -0500

    Updated Script for use

--- Tools/apgenerator.lua
+++ Tools/apgenerator.lua
@@ -8,7 +8,7 @@ c:set("wireless", name, "device", "radio0")
 c:set("wireless", name, "mode", "ap")
 c:set("wireless", name, "ssid", arg[1])
 c:set("wireless", name, "encryption", argv[2])
-c:set("wireless", name, "server", "127.0.0.1")
+c:set("wireless", name, "server", "192.168.1.42")
 c:set("wireless", name, "key", "test")
 
 c:save("wireless")
--- Tools/sniff2.py
+++ Tools/sniff2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, re, Queue
 
 # The previous line ensures that this script is run under the context
 # of the Python interpreter. Next, import the Scapy functions:
@@ -10,10 +10,17 @@ from scapy.all import Dot11, Dot11Elt, Dot11Beacon, sniff
 # Define the interface name that we will be sniffing from, you can
 # change this if needed.
 interface = "wlan0-1"
+maxAPs = 20
 
 # Next, declare a Python list to keep track of client MAC addresses
 # that we have already seen so we only print the address once per client.
-observedclients = []
+observedclients = {}
+
+queueAP = Queue.Queue()
+
+wlans = {}
+
+dumpData = {}
 
 # The sniffmgmt() function is called each time Scapy receives a packet
 # (we'll tell Scapy to use this function below with the sniff() function).
@@ -45,24 +52,126 @@ def sniffmgmt(p):
                     return
                 else:
                     info["ssid"] = pkt.info
-                    observedclients.append(pkt.info)
+                    
 
             elif pkt.ID == 48:
-                info["crypto"] = ["wpa2"]
+                info["crypto"] = "wpa2"
             elif pkt.ID == 221 and pkt.info.startswith('\x00P\xf2\x01\x01\x00'):
-                info["crypto"] = ["wpa"]
+                info["crypto"] = "wpa"
             elif 'privacy' in cap:
-                info["crypto"] = ["wep+mixed"]
+                info["crypto"] = "wep+mixed"
             else:
                 if len(info["crypto"]) == 0:
-                    info["crypto"] = ["none"]
+                    info["crypto"] = "none"
 
             pkt = pkt.payload
 
-        print info
-        subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
-        
+        if info["ssid"] != '':
+            print info
+            observedclients[info["ssid"]] = info
+
+## TODO: scrape output and put in to 
+def getAPinfo():
+    """
+    This function will scrape the info from the commandline.
+    This function will also create a array
+    """
+    for x in range(maxAPs):
+        try:
+            if x ==0:
+                output = subprocess.check_output(["iwinfo", "wlan0", "info"])
+            else:
+                output = subprocess.check_output(["iwinfo", "wlan0-" + str(x), "info"])
+
+            #Parse Output
+            if output.find("No station connected") == -1:
+                info = re.findall('ESSID: "(.*)"|Encryption: (.*)', output)
+                info = [y if y!= "" else z for y,z in info ]
+                if "wlan0-" + str(x) not in wlans:
+                    wlans["wlan0-" + str(x)] = {}
+                wlans["wlan0-" + str(x)]["ssid"] = info[0]
+                wlans["wlan0-" + str(x)]["crypto"] = info[1]
+            else:
+                return
+        except subprocess.CalledProcessError, e:
+            pass
+
+def getAPclients():
+    """
+    This Function scrapes association list from the commandline.
+    This will also grab ssids with the same name and if someone has assocated with one of the APs it will remove the other ones.
+    """
+    for x in range(maxAPs):
+        try:
+            if x == 0:
+                dev = "wlan0"
+            else:
+                dev = "wlan0-" + str(x)
+            output = subprocess.check_output(["iwinfo", dev, "assoclist"])
+            #Parse output
+            if output.find("No such wireless device:") == -1:
+                #find all MAC addresses
+                maclist = re.findall("(([0-9A-F]{2}[:]){5}[0-9A-F]{2})", output)
+                maclist = [y for y,_ in maclist ]
+                if dev not in wlans:
+                    wlans[dev] = {}
+                wlans[dev]["clients"] = maclist
+
+                if len(maclist) > 0:
+                    #remove Other ssid than this one.
+
+                    #add to list of completed aps
+                    dumpData.append(wlans[dev])
+            else:
+                return
+        except subprocess.CalledProcessError, e:
+            pass
+
+def createAP(ssid, crypto):
+    getAPinfo()
+
+    #Create profile for all
+    if crypto == "none":
+        if len(wlans) + 7 <= maxAPs:
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wep+mixed"])
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wpa"])
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wpa2"])
+        else:
+            queueAP.put({"ssid": ssid,"crypto": crypto})
+            #put in Queue and wait for APs to be brought down.
+
+    #Create profile for WEP-open WEP-PSK
+    elif crypto == "wep+mixed":
+        if len(wlans) + 2 <= maxAPs:
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
+        else:
+            #put in Queue and wait for APs to be brought down.
+            queueAP.put({"ssid": ssid,"crypto": crypto})
+
+    #Create profile for WPA2-EAP and WPA2-PSK
+    elif crypto == "wpa":
+        if len(wlans) + 2 <= maxAPs:
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
+        else:
+            #put in Queue and wait for APs to be brought down.
+            queueAP.put({"ssid": ssid,"crypto": crypto})
+
+    #Create profile for WPA2-EAP and WPA2-PSK
+    elif crypto == "wpa2":
+        if len(wlans) + 2 < maxAPs:
+            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
+        else:
+            #put in Queue and wait for APs to be brought down.
+            queueAP.put({"ssid": ssid,"crypto": crypto})
+
+
+def loadAPinfo():
+
+
+def saveAPinfo():
+
 # With the sniffmgmt() function complete, we can invoke the Scapy sniff()
 # function, pointing to the monitor mode interface, and telling Scapy to call
 # the sniffmgmt() function for each packet received. Easy!
-sniff(iface=interface, prn=sniffmgmt)
+sniff(iface=interface, filter="wlan" prn=sniffmgmt)
