3988b9d79ebd713a43eda54d2f3c210befea3446
Tyler Romeo <tylerromeo@gmail.com>
2014-12-10 09:12:21 -0500

    Upgraded Lua AP generator script
    Added logic for specification of key for
    PSK. Also made WPA modes mixed for WPA/WPA2.

--- Tools/apgenerator.lua
+++ Tools/apgenerator.lua
@@ -7,9 +7,24 @@ name = c:add("wireless", "wifi-iface")
 c:set("wireless", name, "device", "radio0")
 c:set("wireless", name, "mode", "ap")
 c:set("wireless", name, "ssid", arg[1])
-c:set("wireless", name, "encryption", arg[2])
-c:set("wireless", name, "server", "192.168.1.42")
-c:set("wireless", name, "key", "test")
+
+-- WEP network
+if argv[2] == "wep" and argv[3] then
+   c:set("wireless", name, "encryption", "wep")
+   c:set("wireless", name, "key", argv[3])
+
+-- WPA-Personal network
+elseif string.sub(argv[2], 0, 3) == "wpa" and argv[3] then
+   c:set("wireless", name, "encryption", "mixed-psk+tkip+ccmp")
+   c:set("wireless", name, "key", argv[3])
+
+-- WPA Enterprise network
+else
+   c:set("wireless", name, "encryption", "mixed-wpa+tkip+ccmp")
+   c:set("wireless", name, "key", "test")
+   c:set("wireless", name, "server", "192.168.1.42")
+
+end
 
 c:save("wireless")
 c:commit("wireless")
