1a01626d13d22d55755a1320e0e07ffd4585ff2f
Brian Ridings <bdrid001@gmail.com>
2014-12-10 05:56:04 -0500

    Updated lua script

--- Tools/apgenerator.lua
+++ Tools/apgenerator.lua
@@ -7,7 +7,7 @@ name = c:add("wireless", "wifi-iface")
 c:set("wireless", name, "device", "radio0")
 c:set("wireless", name, "mode", "ap")
 c:set("wireless", name, "ssid", arg[1])
-c:set("wireless", name, "encryption", argv[2])
+c:set("wireless", name, "encryption", arg[2])
 c:set("wireless", name, "server", "192.168.1.42")
 c:set("wireless", name, "key", "test")
 
