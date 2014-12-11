ec6a5ab6cf468a68747acd644c8af9040e439bcb
Brian Ridings <bdrid001@gmail.com>
2014-12-11 07:05:49 -0500

    Update scripts

--- Tools/apgenerator.lua
+++ Tools/apgenerator.lua
@@ -9,20 +9,25 @@ c:set("wireless", name, "mode", "ap")
 c:set("wireless", name, "ssid", arg[1])
 
 -- WEP network
-if argv[2] == "wep" and argv[3] then
+if arg[2] == "wep" and arg[3] then
    c:set("wireless", name, "encryption", "wep")
-   c:set("wireless", name, "key", argv[3])
-
+   c:set("wireless", name, "key", "1")
+   c:set("wireless", name, "key1", arg[3])
+   
 -- WPA-Personal network
-elseif string.sub(argv[2], 0, 3) == "wpa" and argv[3] then
+elseif string.sub(arg[2], 0, 3) == "wpa" and arg[3] then
    c:set("wireless", name, "encryption", "mixed-psk+tkip+ccmp")
-   c:set("wireless", name, "key", argv[3])
+   c:set("wireless", name, "key", arg[3])
+
+-- Open network
+elseif arg[2] == "none" then
+	c:set("wireless", name, "encryption", "none")
 
 -- WPA Enterprise network
 else
    c:set("wireless", name, "encryption", "mixed-wpa+tkip+ccmp")
-   c:set("wireless", name, "key", "test")
-   c:set("wireless", name, "server", "192.168.1.42")
+   c:set("wireless", name, "auth_server", "192.168.1.42")
+   c:set("wireless", name, "auth_secret", "test")
 
 end
 
--- Tools/sniff2.py
+++ Tools/sniff2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess, re, Queue, pickle, os.path
+import sys, subprocess, re, pickle, os.path, datetime
 
 # The previous line ensures that this script is run under the context
 # of the Python interpreter. Next, import the Scapy functions:
@@ -9,18 +9,19 @@ from scapy.all import Dot11, Dot11Elt, Dot11Beacon, sniff
 
 # Define the interface name that we will be sniffing from, you can
 # change this if needed.
-interface = "wlan0-1"
+if len(sys.argv) > 1:
+    monitor_interface = sys.argv[1]
+else:
+    monitor_interface = "wlan0-1"
 maxAPs = 20
 
 # Next, declare a Python list to keep track of client MAC addresses
 # that we have already seen so we only print the address once per client.
-observedclients = {}
+FoundAPs = {}
 
-queueAP = Queue.Queue()
+currentSpoofedNetworks = {}
 
-wlans = {}
-
-dumpData = {}
+previouslySpoofed = {}
 
 # The sniffmgmt() function is called each time Scapy receives a packet
 # (we'll tell Scapy to use this function below with the sniff() function).
@@ -28,10 +29,6 @@ dumpData = {}
 def sniffmgmt(p):
     info = {}
     info["crypto"] = []
-
-    # Define our tuple (an immutable list) of the 3 management frame
-    # subtypes sent exclusively by clients. I got this list from Wireshark.
-    stamgmtstypes = (4, 10)
 
     # Make sure the packet has the Scapy Dot11 layer present
     if p.haslayer(Dot11Elt):
@@ -46,7 +43,7 @@ def sniffmgmt(p):
         
         while isinstance(pkt, Dot11Elt):
             if pkt.ID == 0:
-                if pkt.info and pkt.info in observedclients:
+                if pkt.info and pkt.info in FoundAPs:
                     return
                 elif pkt.info == '':
                     return
@@ -65,9 +62,10 @@ def sniffmgmt(p):
 
             pkt = pkt.payload
 
-        if info["ssid"] != '':
+        if info != None and info["ssid"] != '':
             print info
-            observedclients[info["ssid"]] = info
+            createAP(info["ssid"], info["crypto"])
+            FoundAPs[info["ssid"]] = info
 
 def getAPinfo():
     """
@@ -83,13 +81,14 @@ def getAPinfo():
             output = subprocess.check_output(["iwinfo", dev, "info"])
 
             #Parse Output
-            if output.find("No station connected") == -1:
+            if output.find("No such wireless device:") == -1 and dev != monitor_interface:
                 info = re.findall('ESSID: "(.*)"|Encryption: (.*)', output)
                 info = [y if y!= "" else z for y,z in info ]
-                if "wlan0-" + str(x) not in wlans:
-                    wlans[dev] = {}
-                wlans[dev]["ssid"] = info[0]
-                wlans[dev]["crypto"] = info[1]
+                if dev not in currentSpoofedNetworks:
+                    currentSpoofedNetworks[dev] = {}
+                print info
+                currentSpoofedNetworks[dev]["ssid"] = info[0]
+                currentSpoofedNetworks[dev]["crypto"] = info[1]
             else:
                 return
         except subprocess.CalledProcessError, e:
@@ -108,76 +107,93 @@ def getAPclients():
                 dev = "wlan0-" + str(x)
             output = subprocess.check_output(["iwinfo", dev, "assoclist"])
             #Parse output
-            if output.find("No such wireless device:") == -1:
+            if output.find("No station connected") == -1:
                 #find all MAC addresses
                 maclist = re.findall("(([0-9A-F]{2}[:]){5}[0-9A-F]{2})", output)
                 maclist = [y for y,_ in maclist ]
-                if dev not in wlans:
-                    wlans[dev] = {}
-                wlans[dev]["clients"] = maclist
+                if dev not in currentSpoofedNetworks and dev != monitor_interface:
+                    currentSpoofedNetworks[dev] = {}
+                    currentSpoofedNetworks[dev]["start"] = datetime.datetime.now()
+                currentSpoofedNetworks[dev]["clients"] = maclist
 
                 if len(maclist) > 0:
                     #remove Other ssid than this one.
 
                     #add to list of completed aps
-                    dumpData.append(wlans[dev])
+                    previouslySpoofed.append(currentSpoofedNetworks[dev])
             else:
                 return
         except subprocess.CalledProcessError, e:
             return
+
+def removeAP(interface):
+    if interface != monitor_interface:
+        subprocess.call(["lua", "removeap.lua", interface])
+
+def removeOldAP():
+    time = datetime.max
+    oldinf = ""
+
+    for inf in currentSpoofedNetworks:
+        if inf != monitor_interface and time > currentSpoofedNetworks[inf]["start"]:
+            time = currentSpoofedNetworks[inf]["start"]
+            oldinf = inf
+    removeAP(oldinf)
 
 def createAP(ssid, crypto):
     getAPinfo()
 
     #Create profile for all
     if crypto == "none":
-        if len(wlans) + 7 <= maxAPs:
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wep+mixed"])
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wpa"])
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], "wpa2"])
-        else:
-            queueAP.put({"ssid": ssid,"crypto": crypto})
-            #put in Queue and wait for APs to be brought down.
+        if len(currentSpoofedNetworks) + 6 > maxAPs:
+           for x in range(len(currentSpoofedNetworks) + 6 - maxAPs):
+                removeOldAP()
+        subprocess.call(["lua", "apgenerator.lua", ssid, crypto])
+        subprocess.call(["lua", "apgenerator.lua", ssid, "wep", "ABCDEF1234567890"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, "wpa", "ABCDEF1234567890"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, "wpa"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, "wpa2", "ABCDEF1234567890"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, "wpa2"])
 
     #Create profile for WEP-open WEP-PSK
     elif crypto == "wep+mixed":
-        if len(wlans) + 2 <= maxAPs:
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
-        else:
-            #put in Queue and wait for APs to be brought down.
-            queueAP.put({"ssid": ssid,"crypto": crypto})
+        if len(currentSpoofedNetworks) + 1 > maxAPs:
+            removeOldAP()
+        subprocess.call(["lua", "apgenerator.lua", ssid, crypto, "ABCDEF1234567890"])
 
     #Create profile for WPA2-EAP and WPA2-PSK
     elif crypto == "wpa":
-        if len(wlans) + 2 <= maxAPs:
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
-        else:
-            #put in Queue and wait for APs to be brought down.
-            queueAP.put({"ssid": ssid,"crypto": crypto})
+        if len(currentSpoofedNetworks) + 2 > maxAPs:
+            for x in range(len(currentSpoofedNetworks) + 2 - maxAPs):
+                removeOldAP()
+        subprocess.call(["lua", "apgenerator.lua", ssid, crypto, "ABCDEF1234567890"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, crypto])
 
     #Create profile for WPA2-EAP and WPA2-PSK
     elif crypto == "wpa2":
-        if len(wlans) + 2 < maxAPs:
-            subprocess.call(["lua", "apgenerator.lua", info["ssid"], info["crypto"]])
-        else:
-            #put in Queue and wait for APs to be brought down.
-            queueAP.put({"ssid": ssid,"crypto": crypto})
+        if len(currentSpoofedNetworks) + 2 > maxAPs:
+            for x in range(len(currentSpoofedNetworks) + 2 - maxAPs):
+                removeOldAP()
+        subprocess.call(["lua", "apgenerator.lua", ssid, crypto, "ABCDEF1234567890"])
+        subprocess.call(["lua", "apgenerator.lua", ssid, info["crypto"]])
 
 
 def loadAPinfo():
     if os.path.isfile("AP.pickle"):
-        dumpData = pickle.load("AP.pickle")
+        previouslySpoofed = pickle.load(open("AP.pickle", "rb+"))
 
 def saveAPinfo():
-    pickle.dump(dumpData, "AP.pickle")
+    pickle.dump(previouslySpoofed, open("AP.pickle", "wb+"))
 
 # With the sniffmgmt() function complete, we can invoke the Scapy sniff()
 # function, pointing to the monitor mode interface, and telling Scapy to call
 # the sniffmgmt() function for each packet received. Easy!
 
 loadAPinfo()
-
-sniff(iface=interface, lfilter=lambda x: x.haslayer(Dot11), prn=sniffmgmt)
+while 1:
+    try:
+        sniff(iface=monitor_interface, lfilter=lambda x: x.haslayer(Dot11) and x.type == 0, prn=sniffmgmt)
+    except socket.error:
+        sleep(10)
 
 saveAPinfo()
